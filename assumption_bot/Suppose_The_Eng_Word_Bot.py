61a997a870e0b5e35e1f97ec99727a4aa4297e31
Michail-Gorev <90262826+Michail-Gorev@users.noreply.github.com>
2023-02-07 23:38:23 +0300

    Угадай перевод с английского

--- assumption_bot/Suppose_The_Eng_Word_Bot.py
+++ assumption_bot/Suppose_The_Eng_Word_Bot.py
@@ -14,21 +14,32 @@ bot: Bot = Bot(BOT_TOKEN)
 dp: Dispatcher = Dispatcher()
 
 # Количество попыток, доступных пользователю в игре
-ATTEMPTS: int = 5
+ATTEMPTS: int = 4
+
+
 
 # Словарь, в котором будут храниться данные пользователя
 user: dict = {'in_game': False,
-              'secret_number': None,
+              'secret_word': '',
               'attempts': None,
               'total_games': 0,
               'wins': 0}
 
 
-# Функция возвращающая случайное целое число от 1 до 100
+# Функция возвращающая случайное целое число от 1 до 2000
 def get_random_number() -> int:
-    return random.randint(1, 100)
-
-
+    return random.randint(0, 1999)
+def get_random_word() -> str:
+    word = []
+    with open('Dictionary.txt', 'r', encoding='utf8') as f:
+        num: int = get_random_number()
+        count: int = 0
+        while count != num:
+            f.readline()
+            count +=1
+        word = f.readline()
+        print(word.split()[1])
+        return word
 # Этот хэндлер будет срабатывать на команду "/start"
 @dp.message(Command(commands=['start']))
 async def process_start_command(message: Message):
@@ -40,8 +51,8 @@ async def process_start_command(message: Message):
 # Этот хэндлер будет срабатывать на команду "/help"
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
-    await message.answer(f'Правила игры:\n\nЯ загадываю число от 1 до 100, '
-                         f'а вам нужно его угадать\nУ вас есть {ATTEMPTS} '
+    await message.answer(f'Правила игры:\n\nЯ загадываю английское слово, '
+                         f'а вам нужно назвать его перевод\nУ вас есть {ATTEMPTS} '
                          f'попыток\n\nДоступные команды:\n/help - правила '
                          f'игры и список команд\n/cancel - выйти из игры\n'
                          f'/stat - посмотреть статистику\n\nДавай сыграем?')
@@ -63,7 +74,7 @@ async def process_cancel_command(message: Message):
                              'снова - напишите об этом')
         user['in_game'] = False
     else:
-        await message.answer('А мы итак с вами не играем. '
+        await message.answer('А мы и так с вами не играем. '
                              'Может, сыграем разок?')
 
 
@@ -72,14 +83,16 @@ async def process_cancel_command(message: Message):
                        'Играть', 'Хочу играть'], ignore_case=True))
 async def process_positive_answer(message: Message):
     if not user['in_game']:
-        await message.answer('Ура!\n\nЯ загадал число от 1 до 100, '
-                             'попробуй угадать!')
+        user['secret_word'] = get_random_word()
+        word_for_print = user['secret_word'].split()[0]
+        await message.answer(f'Ура!\n\nЯ загадал английское слово: {word_for_print}, '
+                             f'попробуй угадать перевод!\n'
+                             f'Ответ пиши без дополнительных знаков, с маленькой буквы!')
         user['in_game'] = True
-        user['secret_number'] = get_random_number()
         user['attempts'] = ATTEMPTS
     else:
         await message.answer('Пока мы играем в игру я могу '
-                             'реагировать только на числа от 1 до 100 '
+                             'реагировать только на русские слова'
                              'и команды /cancel и /stat')
 
 
@@ -91,47 +104,34 @@ async def process_negative_answer(message: Message):
                              'напишите об этом')
     else:
         await message.answer('Мы же сейчас с вами играем. Присылайте, '
-                             'пожалуйста, числа от 1 до 100')
+                             'пожалуйста, перевод слов')
 
 
 # Этот хэндлер будет срабатывать на отправку пользователем чисел от 1 до 100
-@dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
-async def process_numbers_answer(message: Message):
+@dp.message()
+async def process_text_answer(message: Message):
     if user['in_game']:
-        if int(message.text) == user['secret_number']:
-            await message.answer('Ура!!! Вы угадали число!\n\n'
+        #word = user['secret_word'].split()[0]
+        trans = user['secret_word'].split()[1]
+        if str(message.text) == trans:
+            await message.answer('Ура!!! Вы верно назвали перевод!\n\n'
                                  'Может, сыграем еще?')
             user['in_game'] = False
             user['total_games'] += 1
             user['wins'] += 1
-        elif int(message.text) > user['secret_number']:
-            await message.answer('Мое число меньше')
-            user['attempts'] -= 1
-        elif int(message.text) < user['secret_number']:
-            await message.answer('Мое число больше')
+        elif str(message.text) != user['secret_word'].split()[1]:
+            await message.answer('So sad, your option is incorrect( Try again!')
             user['attempts'] -= 1
 
         if user['attempts'] == 0:
             await message.answer(f'К сожалению, у вас больше не осталось '
-                                 f'попыток. Вы проиграли :(\n\nМое число '
-                                 f'было {user["secret_number"]}\n\nДавайте '
+                                 f'попыток. Вы проиграли :(\n\nПеревод слова '
+                                 f'был - {user["secret_word"]}\n\nДавайте '
                                  f'сыграем еще?')
             user['in_game'] = False
             user['total_games'] += 1
     else:
         await message.answer('Мы еще не играем. Хотите сыграть?')
-
-
-# Этот хэндлер будет срабатывать на остальные любые сообщения
-@dp.message()
-async def process_other_text_answers(message: Message):
-    if user['in_game']:
-        await message.answer('Мы же сейчас с вами играем. '
-                             'Присылайте, пожалуйста, числа от 1 до 100')
-    else:
-        await message.answer('Я довольно ограниченный бот, давайте '
-                             'просто сыграем в игру?')
-
 
 if __name__ == '__main__':
     dp.run_polling(bot)
